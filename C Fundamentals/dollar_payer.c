0a1ac32f5f8a016eb11b2c352316e241d4aead82
Adrian Avila <118869551+OiledBanana@users.noreply.github.com>
2024-04-10 21:53:34 +0800

    Update dollar_payer.c

--- C Fundamentals/dollar_payer.c
+++ C Fundamentals/dollar_payer.c
@@ -6,10 +6,18 @@ int amount,twenty_dollar,ten_dollar,five_dollar,one_dollar;
 printf("Enter a dollar amount: \n");
 scanf("%d",&amount);
 twenty_dollar = amount / 20;
-int new_amount = amount - (twenty_dollar * 20);
-ten_dollar = new_amount / 10;
+amount %= 20;
+ten_dollar = amount / 10;
+amount %= 10;
+five_dollar = amount / 5;
+amount %= 5;
+one_dollar = amount;
+
 printf("$20 bills: %d\n",twenty_dollar);
 printf("$10 bills: %d\n",ten_dollar);
+printf("$5 bills: %d\n",five_dollar);
+printf("$1 bills: %d\n", one_dollar);
+
 
 
 
